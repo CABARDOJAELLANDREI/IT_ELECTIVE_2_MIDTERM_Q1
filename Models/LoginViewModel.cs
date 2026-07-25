1c3c1a1f8df9ad678ffdc710cf614d988740494d
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-07-25 11:34:10 +0800

    a

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT_ELECTIVE_BSIT312E_Cabardo_JaellAndrei.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        public string Password { get; set; }
+    }
+}
